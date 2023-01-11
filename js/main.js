79fcab887ea7b16f8c39d4273921e90d026a3b3b
knittenkg <122456200+knittenkg@users.noreply.github.com>
2023-01-11 19:13:40 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(".owl-carousel-1").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:8000,
+        autoplaySpeed:1000,
+        
+    });
+  });
+  $(document).ready(function(){
+    $(".owl-carousel-2").owlCarousel({
+        items:3,
+        margin: 50,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplaySpeed:1000,
+        
+    });
+  });
